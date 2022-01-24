fb1f246f7f8614ea2089067cd955207f2740ee35
Lim Chee Aun <cheeaun@gmail.com>
2022-01-24 09:43:28 +0800

    Use SVG icon instead of ▶️

--- src/app.jsx
+++ src/app.jsx
@@ -60,6 +60,16 @@ const blankBoard = () =>
     Array.from({ length: 4 }, () => ({ v: '', s: null }))
   );
 
+const PlayIcon = (props) => (
+  <svg viewBox="0 0 20 20" fill="currentColor" {...props}>
+    <path
+      fill-rule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+      clip-rule="evenodd"
+    />
+  </svg>
+);
+
 export function App() {
   const [currentGame, setCurrentGame] = useState(
     games.find((g) => g.id === location.hash.slice(1)) || games[0]
@@ -526,7 +536,7 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
                 }
               }}
             >
-              ▶️ Random
+              <PlayIcon width={20} height={20} /> Random
             </button>{' '}
             <button
               type="button"
@@ -544,7 +554,7 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
                 }
               }}
             >
-              ▶️ Choose
+              <PlayIcon width={20} height={20} /> Choose
             </button>
           </p>
         </div>
