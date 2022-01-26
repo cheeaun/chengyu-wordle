7fc87be710536ea12a2aded5bca7a74411539fb0
Lim Chee Aun <cheeaun@gmail.com>
2022-01-26 18:28:28 +0800

    Add tweet button, change link to input field for copy URL

--- src/app.jsx
+++ src/app.jsx
@@ -491,7 +491,8 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
     .join('\n')
     .trim();
   const attempts = gameState === 'won' ? emojiResults.split('\n').length : 'X';
-  const shareText = `Chengyu Wordle [${currentGame.id}] ${attempts}/6\n\n${emojiResults}\n\n${permalink}`;
+  const shareText = `Chengyu Wordle [${currentGame.id}] ${attempts}/6\n\n${emojiResults}`;
+  const shareTextWithLink = `${shareText}\n\n${permalink}`;
 
   const hints = useMemo(() => {
     const hints = [];
@@ -694,15 +695,15 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
                   </a>
                 </small>
               </p>
-              <div class="results">{shareText}</div>
+              <div class="results">{shareTextWithLink}</div>
               <button
                 id="share"
                 onClick={async () => {
                   try {
-                    await navigator.share({ text: shareText });
+                    await navigator.share({ text: shareTextWithLink });
                   } catch (e) {
                     try {
-                      await navigator.clipboard.writeText(shareText);
+                      await navigator.clipboard.writeText(shareTextWithLink);
                       alert('Copied results to clipboard');
                     } catch (e2) {}
                   }
@@ -716,19 +717,57 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
                   ></path>
                 </svg>
               </button>
+              &nbsp;&nbsp;
+              <a
+                class="button tweet"
+                href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                  permalink,
+                )}&text=${encodeURIComponent(shareText)}`}
+                target="_blank"
+              >
+                <svg
+                  height="16"
+                  width="16"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Tweet</title>
+                  <path
+                    fill="currentColor"
+                    d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+                  />
+                </svg>
+              </a>
+              &nbsp;&nbsp;
+              <input
+                readOnly
+                value={currentGame.id}
+                class="idiom-code"
+                onClick={(e) => {
+                  e.target.focus();
+                  e.target.select();
+                  navigator.clipboard
+                    .writeText(permalink)
+                    .then(() => {
+                      alert('Copied URL to clipboard');
+                    })
+                    .catch((e) => {});
+                }}
+              />
             </>
           )}
           <div class="footer">
-            {/won|lost/i.test(gameState) && (
-              <p>
-                <big>
-                  Next idiom:{' '}
-                  <b>
-                    <Countdown />
-                  </b>
-                </big>
-              </p>
-            )}
+            {/won|lost/i.test(gameState) &&
+              getTodayGame().id === currentGame.id && (
+                <p>
+                  <big>
+                    Next idiom:{' '}
+                    <b>
+                      <Countdown />
+                    </b>
+                  </big>
+                </p>
+              )}
             <div>
               {getTodayGame().id !== currentGame.id && (
                 <>
@@ -758,7 +797,10 @@ ${possibleIdioms.map((idiom, i) => `${i + 1}. ${idiom}`).join('\n')}
                 type="button"
                 onClick={() => {
                   // Ask user for idiom ID, load game if ID is valid
-                  const id = prompt('Enter idiom ID:');
+                  let id = prompt('Enter idiom ID/URL:');
+                  try {
+                    id = new URL(id).hash.slice(1);
+                  } catch (e) {}
                   if (id) {
                     const game = games.find((g) => g.id === id);
                     if (game) {
