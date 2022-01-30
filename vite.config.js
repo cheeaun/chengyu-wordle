bfdbb3d18392650714cb15905f980e213500f1cf
Lim Chee Aun <cheeaun@gmail.com>
2022-01-30 09:42:14 +0800

    Add unorm as polyfill for String.normalize

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ import legacy from '@vitejs/plugin-legacy';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [preact(), content(), legacy()],
+  plugins: [
+    preact(),
+    content(),
+    legacy({
+      additionalLegacyPolyfills: ['unorm'],
+    }),
+  ],
   server: {
     port: 3030,
   },
