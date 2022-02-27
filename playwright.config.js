5a41e803e476901c58a03f4f2d70150675a5843e
Lim Chee Aun <cheeaun@gmail.com>
2022-02-28 00:07:09 +0800

    Use https

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4173',
+    baseURL: 'https://localhost:4173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
